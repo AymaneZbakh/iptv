90cd7297f59ba8b508daf75d7f2d1ad7f67665b1
AymaneZbakh <75531985+AymaneZbakh@users.noreply.github.com>
2026-06-12 14:04:58 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ async function startServer() {
   const app = express();
   const PORT = parseInt(process.env.PORT || "3000", 10);
 
-  app.use(express.json());
+  app.use(express.json({ limit: "50mb" }));
 
   // CORS headers
   app.use((req, res, next) => {
